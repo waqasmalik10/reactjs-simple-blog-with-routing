49a56274858dab43296a522f42c79519b29c494c
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-19 13:24:48 +0500

    Navigating Programatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -31,7 +31,9 @@ class Posts extends Component {
     }
 
     postSelectedHandler(id) {
-        this.setState({selectedPostId: id});
+        // this.setState({selectedPostId: id});
+        // this.props.history.push({pathname: '/'+id});
+        this.props.history.push('/'+id);
     }
 
     render() {
@@ -39,9 +41,13 @@ class Posts extends Component {
         if(!this.state.error) {
             posts = this.state.posts.map( post => {
                 return (
-                    <Link to={'/'+post.id} key={post.id}>
-                        <Post title={post.title} author={post.author} clicked={() => this.postSelectedHandler(post.id)}/>
-                    </Link>
+                    // <Link to={'/'+post.id} key={post.id}>
+                        <Post 
+                            key={post.id} 
+                            title={post.title} 
+                            author={post.author} 
+                            clicked={() => this.postSelectedHandler(post.id)}/>
+                    // </Link>
                 )
             });
         }

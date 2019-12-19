1d6df14cbc596111bcc784306de65a84049b28f4
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-19 12:42:20 +0500

    Styling the Active route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import axios from '../../axios'
 import Posts from '../Blog/Posts/Posts'
-import {Route, Link} from 'react-router-dom';
+import {Route, NavLink} from 'react-router-dom';
 
 import FullPost from './FullPost/FullPost';
 import NewPost from './NewPost/NewPost';
-import classes from './Blog.module.css';
+import './Blog.css';
 
 class Blog extends Component {
     render () {
         return (
-            <div className={classes.Blog}>
+            <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                pathname: '/new-post',
-                                // pathname: this.props.match.url + '/new-post',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            <li>
+                                <NavLink 
+                                activeStyle={{color: "#fa923f", textDecoration: "underline"}}
+                                activeClassName="my-active"
+                                to="/" 
+                                exact>Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink 
+                                    exact 
+                                    to={{
+                                    pathname: '/new-post',
+                                    // pathname: this.props.match.url + '/new-post',
+                                    hash: '#submit',
+                                    search: '?quick-submit=true'
+                                }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>

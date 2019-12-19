2d8257fc54f474b74ac985024831d82bb09ffb09
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-19 18:06:36 +0500

    Lazy loading with React Suspense

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
-import {BrowserRouter} from 'react-router-dom'
-
-import Blog from './containers/Blog/Blog';
+import React, { Component, Fragment, Suspense } from 'react';
+import {BrowserRouter, NavLink, Route} from 'react-router-dom'
+import Welcome from './containers/Welcome';
+import User from './containers/User';
+// import Posts from './containers/Posts';
+const Posts = React.lazy(() => import('./containers/Posts'));
+// import Blog from './containers/Blog/Blog';
 
 class App extends Component {
+  state = {
+    show: false
+  }
+
+  modeHandler = () => {
+    this.setState(prevState => {
+      return {show: !prevState.show}
+    }
+    );
+  }
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Blog />
-        </div>
-      </BrowserRouter>
+      <Fragment>
+        <button onClick={this.modeHandler}>Toggle Mode</button>
+        {this.state.show ? <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense> : <User/>}
+      </Fragment>
+      // <BrowserRouter>
+      // <Fragment>
+      //   <nav>
+      //     <NavLink to="/user">User Page</NavLink> | &nbsp;
+      //     <NavLink to="/posts">Posts Page</NavLink>
+      //   </nav>
+      //   <Route path="/" exact component={Welcome} />
+      //   <Route path="/user" component={User} />
+      //   <Route 
+      //     path="/posts" 
+      //     render={() => <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense>} />
+      //   </Fragment>
+      // </BrowserRouter>
     );
   }
 }
